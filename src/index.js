32a5af6f78c00c0e6d3095aa90ac542aed10953d
Hugo Villain <hugo.villain@prismic.io>
2019-08-23 12:14:24 +0200

    Rewrite with bundle size in mind

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const PrismicHelpers = require('prismic-helpers');
 const PrismicRichText = require('prismic-richtext');
 const Component = require('./Component');
 
-console.warn('dev')
 module.exports = {
   Date: PrismicHelpers.Date,
   Elements: PrismicRichText.Elements,
