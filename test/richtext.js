850369a15c3633be5dd81e575225dfbbec13e533
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-07-21 16:17:33 +0200

    fix: tests

--- test/richtext.js
+++ test/richtext.js
@@ -64,7 +64,8 @@ describe('richtext', () => {
       className: "definedClassName",
       width: 250,
     }
-    const wrapper = shallow(renderRichText(richText, null, null, 'div', args));
+    const wrapper = shallow(renderRichText(richText, null, null, 'div', {}, args));
+
     expect(wrapper.prop('width')).to.equal(250);
     expect(wrapper.prop('className')).to.equal('definedClassName');
   })
