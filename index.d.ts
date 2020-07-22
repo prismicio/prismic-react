f8e19de43192e9390b8ba25e61a6438624051ab4
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-07-22 15:19:23 +0200

    update index.d.ts with with sugestions from @schwigri

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,79 @@
-enum Elements {
-  heading1 = 'heading1',
-  heading2 = 'heading2',
-  heading3 = 'heading3',
-  heading4 = 'heading4',
-  heading5 = 'heading5',
-  heading6 = 'heading6',
-  paragraph = 'paragraph',
-  preformatted = 'preformatted',
-  strong = 'strong',
-  em = 'em',
-  listItem = 'list-item',
-  oListItem = 'o-list-item',
-  list = 'group-list-item',
-  oList = 'group-o-list-item',
-  image = 'image',
-  embed = 'embed',
-  hyperlink = 'hyperlink',
-  label = 'label',
-  span = 'span',
+declare module "prismic-reactjs" {
+
+	enum Elements {
+		heading1 = "heading1",
+		heading2 = "heading2",
+		heading3 = "heading3",
+		heading4 = "heading4",
+		heading5 = "heading5",
+		heading6 = "heading6",
+		paragraph = "paragraph",
+		preformatted = "preformatted",
+		strong = "strong",
+		em = "em",
+		listItem = "list-item",
+		oListItem = "o-list-item",
+		list = "group-list-item",
+		oList = "group-o-list-item",
+		image = "image",
+		embed = "embed",
+		hyperlink = "hyperlink",
+		label = "label",
+		span = "span",
+	}
+
+	export type RichTextSpan = {
+		start: number;
+		end: number;
+		type: "strong" | "hyperlink";
+		data?: {
+			link_type: string;
+			url: string;
+			target?: string;
+		};
+	};
+
+	export type RichTextBlock = {
+		type: Elements;
+		text: string;
+		spans: RichTextSpan[];
+	};
+
+	export type HTMLSerializer<T> = (
+		type: React.ElementType,
+		element: any,
+		content: string,
+		children: T[],
+		key: string
+	) => T | null;
+
+	export interface RichTextProps {
+		Component?: React.ReactNode;
+		elements?: {};
+		htmlSerializer?: HTMLSerializer<React.ReactNode>;
+		linkResolver?: () => string;
+		render?: RichTextBlock[];
+		renderAsText?: any;
+		serializeHyperlink?: () => React.ReactNode;
+	}
+
+	export const RichText: React.FC<RichTextProps> & {
+		asText: (input: RichTextBlock[]) => string;
+		render: (input: RichTextBlock[]) => React.ReactNode;
+		displayName: "RichText";
+	};
+
+	interface LinkProps {
+		url(link: any, linkResolver?: (doc: any) => string): string;
+	}
+
+	export const Link: React.FC<LinkProps> & {
+		url: (link: any, linkResolver?: (doc: any) => string) => string;
+	};
+
+	interface PrismicDate {
+		(date?: string): Date;
+	}
+
+	export const Date: PrismicDate;
 }
-
-export type HTMLSerializer<T> = (
-  type: ElementType,
-  element: any,
-  text: string | null,
-  children: T[],
-  index: number
-) => T;
-export interface RichTextProps {
-  Component?: React.ReactNode;
-  htmlSerializer?: HTMLSerializer<T>;
-  linkResolver?: () => string;
-  serializeHyperlink?: () => string;
-  render: () => React.DOMElement;
-}
-
-class RichText extends React.Component<RichTextProps, any> {
-  static asText(text: string): string;
-}
-
-interface Link {
-  url(link: any, linkResolver?: (doc: any) => string): string;
-}
-
-const Link: Link;
-const Date: Date = <string>(): Date => {};
-
-export { RichText, Elements, Link, Date };
