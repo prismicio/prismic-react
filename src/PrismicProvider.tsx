281e27cf59ecd6a9bed3dfc8d7f48184253ad069
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-07-12 15:00:18 -1000

    fix: use more correct internalLinkComponent/externalLinkComponent type

--- src/PrismicProvider.tsx
+++ src/PrismicProvider.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import * as prismic from "@prismicio/client";
 import * as prismicH from "@prismicio/helpers";
 
+import { LinkProps } from "./PrismicLink";
+
 export type PrismicContextValue = {
 	client?: prismic.Client;
 	linkResolver?: prismicH.LinkResolverFunction;
 	richTextComponents?: Record<string, string | React.ComponentType>;
-	internalLinkComponent?: string | React.ComponentType;
-	externalLinkComponent?: string | React.ComponentType;
+	internalLinkComponent?: string | React.ComponentType<LinkProps>;
+	externalLinkComponent?: string | React.ComponentType<LinkProps>;
 	children?: React.ReactNode;
 };
 
