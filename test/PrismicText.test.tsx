7d8b69c9d9dcaf46ac4227f6a967cf2144991a56
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-10-07 14:22:34 -1000

    test: update PrismicText test to match other tests

--- test/PrismicText.test.tsx
+++ test/PrismicText.test.tsx
@@ -1,19 +1,22 @@
-import * as React from "react";
 import test from "ava";
-import { PrismicText } from "../src/PrismicText";
-import field from "./__fixtures__/enRichText.json";
-import { RichTextField } from "@prismicio/types";
-import renderer, { act, ReactTestRenderer } from "react-test-renderer";
+import * as React from "react";
+import * as prismicT from "@prismicio/types";
+
+import { PrismicText } from "../src";
+
+import { renderJSON } from "./__testutils__/renderJSON";
 
 test("returns string when passed RichTextField", (t) => {
-	let tree: ReactTestRenderer;
-	act(() => {
-		tree = renderer.create(<PrismicText field={field as RichTextField} />);
-	});
-	const json = tree!.toJSON();
+	const field: prismicT.RichTextField = [
+		{
+			type: prismicT.RichTextNodeType.heading1,
+			text: "Heading 1",
+			spans: [],
+		},
+	];
 
-	t.is(
-		json,
-		'This block has a line\nbreak just before the word "break" Text before with a multi-block span! Heading 1 bold text italic still bold normal Paragraph A paragraph with a nested label First ordered list item Second ordered list item Third ordered list item Bold and now italic but not anymore value in the last list item Paragraph First list item Second list item Third list item Paragraph A web link A document link A media link A non-image media link A paragraph with a nested document link Paragraph Paragraph',
-	);
+	const actual = renderJSON(<PrismicText field={field} />);
+	const expected = renderJSON(<>Heading 1</>);
+
+	t.deepEqual(actual, expected);
 });
