aa14cbd9c7883ee8c9d8760fcbbce1d334c696c4
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-12-02 16:33:17 -1000

    fix: inject Prismic Toolbar only once

--- src/PrismicToolbar.tsx
+++ src/PrismicToolbar.tsx
@@ -26,19 +26,25 @@ export const PrismicToolbar = ({
 	repositoryName,
 	type = "new",
 }: PrismicToolbarProps): null => {
+	const src = `https://static.cdn.prismic.io/prismic.js?repo=${repositoryName}${
+		type === "new" ? "&type=new" : ""
+	}`;
+
 	React.useEffect(() => {
-		const script = document.createElement("script");
-		script.src = `https://static.cdn.prismic.io/prismic.js?repositoryName=${repositoryName}${
-			type === "new" ? "&type=new" : ""
-		}`;
-		script.defer = true;
+		const existingScript = document.querySelector(`script[src="${src}"]`);
 
-		// Used to distinguish the toolbar element from other elements.
-		script.dataset.prismicToolbar = "";
-		script.dataset.repositoryName = repositoryName;
-		script.dataset.type = type;
+		if (!existingScript) {
+			const script = document.createElement("script");
+			script.src = src;
+			script.defer = true;
 
-		document.body.appendChild(script);
+			// Used to distinguish the toolbar element from other elements.
+			script.dataset.prismicToolbar = "";
+			script.dataset.repositoryName = repositoryName;
+			script.dataset.type = type;
+
+			document.body.appendChild(script);
+		}
 	}, [repositoryName, type]);
 
 	return null;
