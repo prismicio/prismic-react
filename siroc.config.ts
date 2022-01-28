f65ae5d487cc4ec4a246b3d871c4f2a9b66d6cc9
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-01-27 15:31:06 -1000

    chore: ensure an entry file is available for React Native

--- siroc.config.ts
+++ siroc.config.ts
@@ -1,9 +1,28 @@
-import { defineSirocConfig } from "siroc";
+import { defineSirocConfig, PackageJson } from "siroc";
+import fs from "fs";
 
 export default defineSirocConfig({
 	rollup: {
 		output: {
 			sourcemap: true,
+		},
+	},
+	hooks: {
+		"build:done": (pkg) => {
+			// Type assertion is needed to add the `react-native`
+			// key. It is non-standard and not included in
+			// `siroc`'s built-in types.
+			const packageJson = pkg.pkg as PackageJson & {
+				["react-native"]?: string;
+			};
+
+			if (
+				packageJson.module &&
+				packageJson["react-native"] &&
+				pkg.pkg.module !== packageJson["react-native"]
+			) {
+				fs.copyFileSync(packageJson.module, packageJson["react-native"]);
+			}
 		},
 	},
 });
