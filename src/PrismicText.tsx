2e4bab0f81746ee4378c6caa33f71f82f3b3753b
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-05-25 15:18:53 -1000

    fix: throw a helpful error if `<PrismicText>` receives an incorrect field value (#152)
    * fix: throw a helpful error if `<PrismicText>` receives an incorrect field value
    
    * fix: rename message slug

--- src/PrismicText.tsx
+++ src/PrismicText.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import * as prismicT from "@prismicio/types";
 import * as prismicH from "@prismicio/helpers";
 
+import { __PRODUCTION__ } from "./lib/__PRODUCTION__";
+import { devMsg } from "./lib/devMsg";
+
 /**
  * Props for `<PrismicText>`.
  */
@@ -42,6 +45,16 @@ export type PrismicTextProps = {
  * @see Learn about Rich Text fields {@link https://prismic.io/docs/core-concepts/rich-text-title}
  */
 export const PrismicText = (props: PrismicTextProps): JSX.Element | null => {
+	if (!__PRODUCTION__) {
+		if (typeof props.field === "string") {
+			throw new Error(
+				`[PrismicText] The "field" prop only accepts a Rich Text or Title field's value but was provided a different type of field instead (e.g. a Key Text or Select field). You can resolve this error by rendering the field value inline without <PrismicText>. For more details, see ${devMsg(
+					"prismictext-works-only-with-rich-text-and-title-fields",
+				)}`,
+			);
+		}
+	}
+
 	return React.useMemo(() => {
 		if (prismicH.isFilled.richText(props.field)) {
 			const text = prismicH.asText(props.field, props.separator);
