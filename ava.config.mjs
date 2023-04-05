92a00425068c464d8656bdb1c11f1cef379e0281
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-04-04 14:12:23 -1000

    chore: remove `ava.config.mjs`

--- ava.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-	extensions: ["ts", "tsx"],
-	files: ["./test/**/*.test.ts", "./test/**/*.test.tsx"],
-	require: ["esbuild-register", "global-jsdom/register"],
-	verbose: true,
-	timeout: "60s",
-};
