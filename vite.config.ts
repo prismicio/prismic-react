fdff6ce800407ad19e440863a23ffb19afd0137d
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-05-01 09:06:19 -1000

    fix: bundle `@prismicio/client` v7 to prevent `npm install` issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 import preserveDirectives from "rollup-plugin-preserve-directives";
 
 export default defineConfig({
-	plugins: [sdk(), react()],
+	plugins: [
+		sdk({
+			internalDependencies: ["@prismicio/client"],
+		}),
+		react(),
+	],
 	build: {
 		lib: {
 			entry: {
